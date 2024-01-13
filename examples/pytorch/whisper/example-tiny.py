122d28ece7f198be7fdfff0e80aeb1896192c015
Edgar Lin <edgarlin2000@gmail.com>
2024-01-13 05:26:39 +0000

    Fixed seq len check error

--- examples/pytorch/whisper/example-tiny.py
+++ examples/pytorch/whisper/example-tiny.py
@@ -3,21 +3,21 @@ import torch
 import sys
 from transformers import AutoProcessor
 from utils import WhisperForConditionalGeneration
+from utils import FTWhisperForConditionalGeneration
 from datasets import load_dataset
+import numpy as np
 
 processor = AutoProcessor.from_pretrained("openai/whisper-tiny")
-model = WhisperForConditionalGeneration.from_pretrained("openai/whisper-tiny")
+model = FTWhisperForConditionalGeneration.from_pretrained("openai/whisper-tiny")
+model.cuda(5,5)
 
 
 
 ds = load_dataset("hf-internal-testing/librispeech_asr_dummy", "clean", split="validation")
-
-inputs = processor(ds[0]["audio"]["array"], return_tensors="pt")
-
-input_features = inputs.input_features
-
-generated_ids = model.generate(inputs=input_features)
-
-transcription = processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
-print(f"Transcription:{transcription}")
+for i in range(0,10):
+    input_features = torch.from_numpy(np.stack(processor([item["array"] for item in ds[5*i:5*i+5]["audio"]]).input_features))
+    print(input_features.shape)
+    generated_ids = model.generate(inputs=input_features)
+    transcription = processor.batch_decode(generated_ids, skip_special_tokens=True)
+    print(f"Transcription:{transcription}")
 # %%
